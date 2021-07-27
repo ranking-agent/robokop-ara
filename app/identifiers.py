f8a18bac5aa0a27ea2e6ace05c674dd9078e9391
Patrick Wang <patrickkwang@users.noreply.github.com>
2021-07-27 16:51:09 -0400

    Re-org a bit

---
+++ app/identifiers.py
@@ -0,0 +1,67 @@
+"""Utilities for handling identifiers."""
+from fastapi.exceptions import HTTPException
+import httpx
+
+
+async def get_synonyms(curies: list[str]):
+    """Get synonyms for CURIE."""
+    async with httpx.AsyncClient() as client:
+        response = await client.post(
+            "https://nodenormalization-sri.renci.org/1.1/get_normalized_nodes",
+            json={"curies": curies},
+        )
+        if response.status_code != 200:
+            raise HTTPException(500, f"Failed synonymizing node ids: {response.text}")
+
+    return {
+        key: {
+            "synonyms": [eq_id["identifier"] for eq_id in value["equivalent_identifiers"]],
+            "categories": value["type"],
+        }
+        for key, value in response.json().items()
+    }
+
+
+PREFERRED_PREFIXES = None
+async def get_preferred_prefixes():
+    """Get preferred prefixes from automat/robokopkg."""
+    global PREFERRED_PREFIXES
+    if PREFERRED_PREFIXES is None:
+        async with httpx.AsyncClient() as client:
+            response = await client.get(
+                "https://automat.renci.org/robokopkg/1.1/meta_knowledge_graph",
+            )
+            if response.status_code != 200:
+                raise HTTPException(500, f"Failed finding preferred prefixes: {response.text}")
+        meta_kg = response.json()
+        PREFERRED_PREFIXES = {
+            category: value["id_prefixes"]
+            for category, value in meta_kg["nodes"].items()
+        }
+    return PREFERRED_PREFIXES
+
+
+async def map_identifiers(trapi_query):
+    """Map identifiers to preferred set."""
+    preferred_prefixes = await get_preferred_prefixes()
+    curies = [
+        curie
+        for node in trapi_query["message"]["query_graph"]["nodes"].values()
+        for curie in node.get("ids", [])
+    ]
+    synonyms = await get_synonyms(curies)
+    for node in trapi_query["message"]["query_graph"]["nodes"].values():
+        if not node.get("ids"):
+            continue
+        node["ids"] = [
+            next(
+                synonym
+                for synonym in synonyms[curie]["synonyms"]
+                if any(
+                    synonym.startswith(prefix)
+                    for category in synonyms[curie]["categories"]
+                    for prefix in preferred_prefixes[category]
+                )
+            )
+            for curie in node["ids"]
+        ]
--- app/server.py
+++ app/server.py
@@ -9,6 +9,7 @@ import httpx
 from reasoner_pydantic import Query as ReasonerQuery, Response
 from starlette.requests import Request
 
+from .identifiers import map_identifiers
 from .util import load_example
 from .trapi import TRAPI
 
@@ -40,70 +41,6 @@ async def exception_handler(request: Request, exc: Exception):
         status_code=500,
         content={"message": str(exc)},
     )
-
-
-async def get_synonyms(curies: list[str]):
-    """Get synonyms for CURIE."""
-    async with httpx.AsyncClient() as client:
-        response = await client.post(
-            "https://nodenormalization-sri.renci.org/1.1/get_normalized_nodes",
-            json={"curies": curies},
-        )
-        if response.status_code != 200:
-            raise HTTPException(500, f"Failed synonymizing node ids: {response.text}")
-
-    return {
-        key: {
-            "synonyms": [eq_id["identifier"] for eq_id in value["equivalent_identifiers"]],
-            "categories": value["type"],
-        }
-        for key, value in response.json().items()
-    }
-
-
-PREFERRED_PREFIXES = None
-async def get_preferred_prefixes():
-    """Get preferred prefixes from automat/robokopkg."""
-    global PREFERRED_PREFIXES
-    if PREFERRED_PREFIXES is None:
-        async with httpx.AsyncClient() as client:
-            response = await client.get(
-                "https://automat.renci.org/robokopkg/1.1/meta_knowledge_graph",
-            )
-            if response.status_code != 200:
-                raise HTTPException(500, f"Failed finding preferred prefixes: {response.text}")
-        meta_kg = response.json()
-        PREFERRED_PREFIXES = {
-            category: value["id_prefixes"]
-            for category, value in meta_kg["nodes"].items()
-        }
-    return PREFERRED_PREFIXES
-
-
-async def map_identifiers(trapi_query):
-    """Map identifiers to preferred set."""
-    preferred_prefixes = await get_preferred_prefixes()
-    curies = [
-        curie
-        for node in trapi_query["message"]["query_graph"]["nodes"].values()
-        for curie in node.get("ids", [])
-    ]
-    synonyms = await get_synonyms(curies)
-    for node in trapi_query["message"]["query_graph"]["nodes"].values():
-        if not node.get("ids"):
-            continue
-        node["ids"] = [
-            next(
-                synonym
-                for synonym in synonyms[curie]["synonyms"]
-                if any(
-                    synonym.startswith(prefix)
-                    for category in synonyms[curie]["categories"]
-                    for prefix in preferred_prefixes[category]
-                )
-            )
-            for curie in node["ids"]
-        ]
 
 
 @APP.post(
