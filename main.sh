39352789e909fe8839930a3f1d5c3cf0b00ac7a8
Patrick Wang <patrickkwang@users.noreply.github.com>
2021-07-28 13:10:15 -0400

    Allow specific root path: env var ROOT_PATH

--- main.sh
+++ main.sh
@@ -2,4 +2,8 @@
 
 export $(egrep -v '^#' .env | xargs)
 
-uvicorn app.server:APP --host 0.0.0.0 --port $PORT
+if [[ -z "${ROOT_PATH}" ]]; then
+    uvicorn app.server:APP --host 0.0.0.0 --port $PORT
+else
+    uvicorn app.server:APP --host 0.0.0.0 --port $PORT --root-path $ROOT_PATH
+fi
